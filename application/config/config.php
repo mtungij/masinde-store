5e8aa902042aa600b4c2883d35b7fd6283d97b55
alkadoHs <alkadosichone@gmail.com>
2023-09-25 14:43:10 -0700

    added ip

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://144.91.121.229/masinde/';
+$config['base_url'] = 'http://144.91.121.229/';
 
 /*
 |--------------------------------------------------------------------------
