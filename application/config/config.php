767dd562f6304252237570a736507a110246251a
alkadoHs <alkadosichone@gmail.com>
2023-09-25 17:06:00 -0700

    dmn

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://144.91.121.229/';
+$config['base_url'] = 'https://masindestore.com/';
 
 /*
 |--------------------------------------------------------------------------
