bdc19debfc190c9a327fba62fc37a1ff0d07cc6b
jonson <jamesmtungi2013@gmail.com>
2023-10-23 00:01:37 +0300

    update

--- application/views/expenses/salary.php
+++ application/views/expenses/salary.php
@@ -101,8 +101,8 @@
                       <div class="relative z-0">
                         <select id="branch_id" name="income" value="<?= set_value('income'); ?>" class="w-full h-12 block leading-5 relative py-2 px-4 rounded bg-neutral-10 dark:bg-neutral-900 border focus:border-2 border-gray-500 overflow-x-auto focus:outline-none focus:border-primary-600 focus:ring-0 dark:text-gray-200 dark:border-gray-400 dark:focus:border-primary-200">
                           <option value="">Select income type</option>
-                          <option value="1">Capital</option>
-                          <option value="2">Profit</option>
+                          <option value="capital">Capital</option>
+                          <option value="profit">Profit</option>
                         </select>
                         <label for="branch_id" class="absolute tracking-[.03125em] text-gray-500 dark:text-gray-400 bg-neutral-10 dark:bg-neutral-900 duration-300 transform px-1 -translate-y-6 scale-75 top-3 z-10 origin-[0] left-4 peer-focus:left-4 peer-focus:text-primary-600 dark:peer-focus:text-primary-200 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:bg-neutral-10 dark:peer-focus:bg-neutral-900 peer-focus:px-1 peer-invalid:text-error-600 dark:peer-invalid:text-error-200">Income Type</label>
                       </div> 
